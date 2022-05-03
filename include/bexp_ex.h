fcae4a8e868e5ff6877bd1fd4f197816ffa7eb73
Deak Bergamote <43889049+CordovaJuarez-DanielEduardo@users.noreply.github.com>
2022-05-02 20:26:51 -0700

    Create bexp_ex.h
    Se agregó todo lo relacionado a "bexp_ex_t" desde el archivo include/imp.h, junto a su guarda de inclusión ED_BEXP_EX_H_

---
+++ include/bexp_ex.h
@@ -0,0 +1,27 @@
+#ifndef ED_BEXP_EX_H_
+#define ED_BEXP_EX_H_
+
+#include <bexp.h>
+#include <aexp.h>
+
+/*** EXTENSIONES DE EXPRESIONES BOOLEANAS ***/
+
+struct pexp_ex_t;
+typedef struct pexp_ex_t pexp_ex_t;
+
+bool bexp_ex_is_not_equal(bexp_ex_t *b);
+bool bexp_ex_is_less_equal(bexp_ex_t *b);
+bool bexp_ex_is_great(bexp_ex_t *b);
+bool bexp_ex_is_great_equal(bexp_ex_t *b);
+
+
+bexp_ex_t *bexp_make_not_equal(aexp_t *left, aexp_t *right);
+bexp_ex_t *bexp_make_less_equal(aexp_t *left, aexp_t *right);
+bexp_ex_t *bexp_make_great(aexp_t *left, aexp_t *right);
+bexp_ex_t *bexp_make_great_equal(aexp_t *left, aexp_t *right);
+
+bexp_t *bexp_ex_eval(bexp_ex_t *b);
+
+void bexp_ex_free(bexp_ex_t *b);
+
+#endif  ED_BEXP_EX_H_
