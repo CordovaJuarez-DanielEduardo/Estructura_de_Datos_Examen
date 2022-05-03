f692217d6c9c567b296a376bdcfe504702f4e7a1
Deak Bergamote <43889049+CordovaJuarez-DanielEduardo@users.noreply.github.com>
2022-05-02 21:20:53 -0700

    Update bexp_ex.c

--- src/bexp_ex.c
+++ src/bexp_ex.c
@@ -85,14 +85,14 @@ bexp_ex_t *bexp_make_great_equal(aexp_t *left, aexp_t *right) {
 bexp_ex_t *bexp_ex_eval(bexp_ex_t *b) {
     bexp_t *RETURN = NULL;
     if(bexp_ex_is_not_equal(b)){
-        bexp_t *equal = bexp_make_equal(b->left, b->right);
+        bexp_t *equal = bexp_make_equal(bexp_ex_left(b), bexp_ex_right(b));
         RETURN = bexp_make_neg(equal);
         if(RETURN == NULL) bexp_free(equal);
     }
     if(bexp_ex_is_less_equal(b)){
         bexp_t *less, *equal;
-        equal = bexp_make_equal(b->left, b->right);
-        less = bexp_make_less(b->left, b->right);
+        equal = bexp_make_equal(bexp_ex_left(b), bexp_ex_right(b));
+        less = bexp_make_less(bexp_ex_left(b), bexp_ex_right(b));
         RETURN = bexp_make_or(less, equal);
         
         if(RETURN == NULL){
@@ -101,10 +101,10 @@ bexp_ex_t *bexp_ex_eval(bexp_ex_t *b) {
         }
     }
     if(bexp_ex_is_great(b)){
-        RETURN = bexp_make_less(b->right, b->left);
+        RETURN = bexp_make_less(bexp_ex_left(b), bexp_ex_right(b));
     }
     if(bexp_ex_is_great_equal(b)){
-        bexp_t *less = bexp_make_less(b->left, b->right);
+        bexp_t *less = bexp_make_less(bexp_ex_left(b), bexp_ex_right(b));
         RETURN = bexp_make_neg(less);
         if(RETURN == NULL) bexp_free(less);
     }
