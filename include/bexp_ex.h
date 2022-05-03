5aa69830f9c42550109da50016a1e2565e04555d
Deak Bergamote <43889049+CordovaJuarez-DanielEduardo@users.noreply.github.com>
2022-05-02 20:58:53 -0700

    Update bexp_ex.h
    Se agregaron #include, y corrigió la definición del struct de "pexp..." a "bexp..."
    
    Falta documentar

--- include/bexp_ex.h
+++ include/bexp_ex.h
@@ -1,13 +1,18 @@
 #ifndef ED_BEXP_EX_H_
 #define ED_BEXP_EX_H_
 
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <inttypes.h>
 #include <bexp.h>
 #include <aexp.h>
+#include <mexp.h>
 
 /*** EXTENSIONES DE EXPRESIONES BOOLEANAS ***/
 
-struct pexp_ex_t;
-typedef struct pexp_ex_t pexp_ex_t;
+struct bexp_ex_t;
+typedef struct bexp_ex_t bexp_ex_t;
 
 bool bexp_ex_is_not_equal(bexp_ex_t *b);
 bool bexp_ex_is_less_equal(bexp_ex_t *b);
