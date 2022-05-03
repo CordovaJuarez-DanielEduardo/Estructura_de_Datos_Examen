39e762808b49cd43ecf321d9d563abcd4d525e72
Deak Bergamote <43889049+CordovaJuarez-DanielEduardo@users.noreply.github.com>
2022-05-03 12:41:15 -0700

    Update bexp_ex.c

--- src/bexp_ex.c
+++ src/bexp_ex.c
@@ -1,88 +1,6 @@
 #include <bexp_ex.h>
 
 
-/** VERSION EXTENDIDA DE EXPRESIONES BOOLEANAS **/
-typedef enum{
-    BEXP_EX_NOT_EQUAL,
-    BEXP_EX_LESS_EQUAL,
-    BEXP_EX_GREAT,
-    BEXP_EX_GREAT_EQUAL
-}BEXP_EX_TYPE;
-
-typedef struct bexp_ex_t{
-    BEXP_EX_TYPE type;
-    struct aexp_t *left;
-    struct aexp_t *right;
-} bexp_ex_t;
-
-/* PREDICADOS */
-bool bexp_ex_is_not_equal(bexp_ex_t *b) {
-    return b->type == BEXP_EX_NOT_EQUAL;
-}
-
-bool bexp_ex_is_less_equal(bexp_ex_t *b) {
-    return b->type == BEXP_EX_LESS_EQUAL;
-}
-
-bool bexp_ex_is_great(bexp_ex_t *b) {
-    return b->type == BEXP_EX_GREAT;
-}
-
-bool bexp_ex_is_great_equal(bexp_ex_t *b) {
-    return b->type == BEXP_EX_GREAT_EQUAL;
-}
-
-aexp_t *bexp_ex_left(bexp_ex_t *b) {
-    return b->left;
-}
-
-aexp_t *bexp_ex_right(bexp_ex_t *b) {
-    return b->right;
-}
-
-/* CONSTRUCTORES */
-
-bexp_ex_t *bexp_make_not_equal(aexp_t *left, aexp_t *right) {
-    bexp_t *root = (bexp_t *)malloc(sizeof(bexp_t));
-    if (root == NULL) return NULL;
-    root->type = BEXP_EX_NOT_EQUAL;
-
-    root->left = left;
-    root->right = right;
-    return root;
-}
-
-bexp_ex_t *bexp_make_less_equal(aexp_t *left, aexp_t *right) {
-    bexp_t *root = (bexp_t *)malloc(sizeof(bexp_t));
-    if(root == NULL) return NULL;
-    root->type = BEXP_EX_LESS_EQUAL;
-
-    root->left = left;
-    root->right = right;
-    return root;
-}
-
-bexp_ex_t *bexp_make_great(aexp_t *left, aexp_t *right) {
-    bexp_t *root = (bexp_t *)malloc(sizeof(bexp_t));
-    if(root == NULL) return NULL;
-    root->type = BEXP_EX_GREAT;
-
-    root->left = left;
-    root->right = right;
-    return root;
-}
-
-bexp_ex_t *bexp_make_great_equal(aexp_t *left, aexp_t *right) {
-    bexp_t *root = (bexp_t *)malloc(sizeof(bexp_t));
-    if(root == NULL) return NULL;
-    root->type = BEXP_EX_GREAT_EQUAL;
-
-    root->left = left;
-    root->right = right;
-    return root;
-}
-
-
 //EVALUADOR DE EXPRESIONES BOOLEANAS EXTENDIDAS
 bexp_t *bexp_ex_eval(bexp_ex_t *b) {
     bexp_t *RETURN = NULL;
@@ -99,7 +17,7 @@ bexp_t *bexp_ex_eval(bexp_ex_t *b) {
 
         if(RETURN == NULL){
             bexp_free(equal);
-            bexo_free(less);
+            bexp_free(less);
         }
     }
     if(bexp_ex_is_great(b)){
