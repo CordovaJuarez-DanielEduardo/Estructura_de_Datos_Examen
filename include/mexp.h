37903a578614559f60006e9932054b5cff50e3c3
Juanito773 <104812302+Juanito773@users.noreply.github.com>
2022-05-03 10:57:06 -0700

    Update mexp.h
    Doc

--- include/mexp.h
+++ include/mexp.h
@@ -10,9 +10,11 @@
 /*  MEMORIA  */
 /*************/
 
+// Una estructura de tipo 'mexp_t' representa una expresión de tipo memoria.
 struct mexp_t;
 typedef struct mexp_t mexp_t;
 
+// Una estructura de tipo 'nodo' representa una expresión de tipo nodo.
 struct nodo;
 typedef struct nodo nodo;
 
@@ -20,14 +22,20 @@ typedef struct nodo nodo;
 nodo *mem_make_nodo(uint64_t indice);
 nodo *mem_make_nodo(uint64_t indice, uint64_t val);
 
+//Asigna lista a los nodos izquierdo y derecho según su índice.
 nodo *mexp_lista(mexp_t *x);
 nodo *mexp_add(nodo *n, mexp_t *x);
 nodo *mexp_add(nodo *n, nodo *l);
-  
+
+/* Busca si existe el nodo en un índice n del nodo para ver si es mejor, mayor o igual
+ a un índice i, y si son diferentes a NULL, para así obtener el valor según 
+Ssu índice y lista.
+*/
 nodo *mexp_busca(aexp_t *indice, mexp_t *x);
 nodo *mexp_busca(uint64_t i, nodo *n);
 uint64_t mexp_obten_val(aexp_t i, mexp_t *x);
 
+// Respectivas asignaciones de los índices, nodos izquierdo y derecho, así como sus listas.
 uint64_t nodo_indice(nodo *n);
 uint64_t nodo_val(nodo *n);
 nodo *nodo_left(mexp_t *x);
